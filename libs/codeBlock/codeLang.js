fa954391b6431460042498b7d2bc55dd94740c73
ysqq4 <3290308461@qq.com>
2023-11-30 23:50:38 +0800

    Site updated: 2023-11-30 23:50:33

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -2,8 +2,8 @@
 
 $(function () {
   var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
-
-  $('pre').before($highlight_lang);
+// 把before改成了after
+  $('pre').after($highlight_lang);
   $('pre').each(function () {
     var code_language = $(this).attr('class');
 
@@ -13,7 +13,8 @@ $(function () {
     var lang_name = code_language.replace("line-numbers", "").trim().replace("language-", "").trim();
 
     // 首字母大写
-    // lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
+    // 下面一行取消了注释
+    lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
     
     $(this).siblings(".code_lang").text(lang_name);
   });
